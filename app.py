d72b737188f821a8af4f1ab3d9532c20965fdd4e
santhy123 <santhyrenjith95@gmail.com>
2019-02-27 12:37:20 +0530

    host page

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "HELLO.... I AM SANTHY"
+@app.route("/home")
+def home():
+    return "welcome to my home page"
+@app.route("/contact")
+def contact():
+    return "contact the page"
+@app.route("/abort")
+def abort():
+    return "abort the page"
 if (__name__=="__main__"):
     app.run()
